1bca67c9d00dc33686b5b0c48541f99a75265e31
SeungminShin97 <107799398+SeungminShin97@users.noreply.github.com>
2022-10-20 23:01:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 212 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextBigInteger().add(sc.nextBigInteger().add(sc.nextBigInteger())));
+        sc.close();
+    }
+}
