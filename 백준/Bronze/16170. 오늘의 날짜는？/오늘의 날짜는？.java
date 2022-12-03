da9976256e8a79dc36daee6bbc4b7a23807e624d
SeungminShin97 <107799398+SeungminShin97@users.noreply.github.com>
2022-12-03 16:18:19 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 208 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now.format(DateTimeFormatter.ofPattern("yyyy\nMM\ndd")));       
+    }
+}
