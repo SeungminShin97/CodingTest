deb05660e9c00ed1ed0fea4db844c9ee4302261a
SeungminShin97 <107799398+SeungminShin97@users.noreply.github.com>
2023-01-06 18:04:34 +0900

    [Bronze V] Title: Pups, Time: 132 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            double d = Double.parseDouble(st.nextToken());
+            double f = Double.parseDouble(st.nextToken());
+            double p = Double.parseDouble(st.nextToken());
+            sb.append(String.format("$%.2f", d * f * p));
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
