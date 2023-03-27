d86e588c2e560ff94996b56f67497643d410ecf5
SeungminShin97 <107799398+SeungminShin97@users.noreply.github.com>
2023-03-27 23:35:08 +0900

    [Bronze V] Title: Can you add this?, Time: 144 ms, Memory: 15804 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            sb.append(a + b + "\n");
+        }
+        System.out.println(sb);
+    }
+}
