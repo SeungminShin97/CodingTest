2d29a1585d9b5f57401b40cef016a4179e4a0f77
SeungminShin97 <107799398+SeungminShin97@users.noreply.github.com>
2024-01-04 10:51:20 +0900

    [level 2] Title: 올바른 괄호, Time: 19.05 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        if(s.charAt(0) == ')')
+            return false;
+        stack.add(s.charAt(0));
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == '(')
+                stack.add(s.charAt(i));
+            else {
+                if(stack.size() == 0)
+                    return false;
+                stack.pop();
+            }
+        }
+        return (stack.size() == 0) ? true : false;
+    }
+}
